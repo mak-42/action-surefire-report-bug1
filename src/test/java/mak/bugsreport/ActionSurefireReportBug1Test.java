d2887e0db507cf41252b0719b88e0b052fd46369
Dmitry Malenok <_mak_@bk.ru>
2023-10-31 23:44:50 +0300

    Initial commit

---
+++ src/test/java/mak/bugsreport/ActionSurefireReportBug1Test.java
@@ -0,0 +1,13 @@
+package mak.bugsreport;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ActionSurefireReportBug1Test {
+
+    @Test
+    public void test1(){
+        String expectedString = "value";  
+        assertEquals(expectedString, "value"); 
+    }
+}
