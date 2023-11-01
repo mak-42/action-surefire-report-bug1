213214f0a3713fcd34c48cbd38448ac392ff4dbe
Tester1Git <149560746+Tester1Git@users.noreply.github.com>
2023-11-01 05:47:08 +0000

    Update ActionSurefireReportBug1Test.java

--- src/test/java/mak/bugsreport/ActionSurefireReportBug1Test.java
+++ src/test/java/mak/bugsreport/ActionSurefireReportBug1Test.java
@@ -7,7 +7,7 @@ public class ActionSurefireReportBug1Test {
 
     @Test
     public void test1(){
-        String expectedString = "value";  
+        String expectedString =  "value";  
         assertEquals(expectedString, "value"); 
     }
 }
